572da267a34a7cd277c85f882a57f91f5b85d3c6
suhaibvazin <suhaibvazin321@gmail.com>
2024-03-16 01:22:44 +0530

    optimise imports

--- src/main/java/com/example/Hidrator/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/example/Hidrator/service/impl/AuthenticationServiceImpl.java
@@ -13,8 +13,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
