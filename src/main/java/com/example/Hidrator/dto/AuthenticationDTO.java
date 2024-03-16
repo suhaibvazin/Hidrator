d8a88fb12a0860bef785dc68b0fef73b157b564b
suhaibvazin <suhaibvazin321@gmail.com>
2024-03-16 15:05:23 +0530

    code clean up

--- src/main/java/com/example/Hidrator/dto/AuthenticationDTO.java
+++ src/main/java/com/example/Hidrator/dto/AuthenticationDTO.java
@@ -2,8 +2,6 @@ package com.example.Hidrator.dto;
 
 
 import com.example.Hidrator.enums.Role;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,16 +9,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AuthenticationDTO {
-    @NotBlank(message = "firstname cant be empty")
+
     private String firstName;
-    @NotBlank(message = "lastname cant be empty")
     private String lastName;
-    @NotBlank(message = "username cant be empty")
     private String username;
-    @NotBlank(message = "password cant be empty")
-    @Size(min=8, max=25)
     private String password;
-    @NotBlank(message = "role cant be empty")
     private Role role;
     private String token;
 }
